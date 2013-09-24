42ddb46634b113af656f8d849b40dd7816efa587
Morgan Packard <morgan@morganpackard.com>
2013-09-24 12:53:32 -0400

    Update SHK.m
    The isUsingCocoaPods was forcing a redundant addition of ShareKit.bundle to the path, giving me ShareKit.bundle/ShareKit.bundle.

--- Classes/ShareKit/Core/SHK.m
+++ Classes/ShareKit/Core/SHK.m
@@ -719,11 +719,11 @@ NSString* SHKLocalizedStringFormat(NSString* key)
   if (nil == bundle) {
       
       NSString *path = nil;
-      if ([SHKCONFIG(isUsingCocoaPods) boolValue]) {
+      //if ([SHKCONFIG(isUsingCocoaPods) boolValue]) {
           path = [SHK shareKitLibraryBundlePath];
-      } else {
-          path = [[SHK shareKitLibraryBundlePath] stringByAppendingPathComponent:@"ShareKit.bundle"];
-      }
+      //} else {
+      //    path = [[SHK shareKitLibraryBundlePath] stringByAppendingPathComponent:@"ShareKit.bundle"];
+      //}
       
       bundle = [NSBundle bundleWithPath:path];
       NSCAssert(bundle != nil,@"ShareKit has been refactored to be used as Xcode subproject. Please follow the updated installation wiki and re-add it to the project. Please do not forget to clean project and clean build folder afterwards. In case you use CocoaPods override - (NSNumber *)isUsingCocoaPods; method in your configurator subclass and return [NSNumber numberWithBool:YES]");
